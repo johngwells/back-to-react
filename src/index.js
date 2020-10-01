65dcb0e9641f9cccc57b97e302c002f861f128a8
John Wells <molekewl2017@gmail.com>
2020-09-30 21:09:02 -0700

    jsx & basic elements of react

--- src/index.js
+++ src/index.js
@@ -2,12 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Create Component
-const App = function() {
-  return <div>Hello World!</div>
-}
+const App = () => {
+  const buttonText = { text: 'Touch Me' };
+
+  return (
+    <div>
+      <h1 style={{ color: 'skyblue' }}>Hello World!</h1>
+      <label className='label' htmlFor='name'>
+        Enter Name:
+      </label>
+      <input id='name' type='text'></input>
+      <button
+        style={{
+          backgroundColor: 'skyblue',
+          color: 'white',
+          border: 'gray',
+          width: '100px',
+          height: '25px',
+          borderRadius: '5px'
+        }}
+      >
+        {buttonText.text}
+      </button>
+    </div>
+  );
+};
 
 // Show on screen
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-)
+ReactDOM.render(<App />, document.querySelector('#root'));
